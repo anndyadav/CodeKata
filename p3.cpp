41b7e4c74f9dc781fb3cef0b78c21de22626579b
Anand Yadav <yanand64@gmail.com>
2017-03-11 19:25:29 +0530

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+/*
+    *
+    * Anand Yadav
+    *
+*/
+
+
+using namespace std;
+
+int main(){
+	char t;
+	cin>>t;
+	if(t=='a'||t=='e'||t=='i'||t=='o'||t=='u'){
+		cout<<"vowel";
+	}
+	else{
+		cout<<"consonant";
+	}
+
+return 0;
+
+}
+
